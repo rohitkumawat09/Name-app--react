d629a0c2c0c00347a59e63ec7fa108e4191e2daf
Your Name <your-email@example.com>
2025-04-22 00:00:36 -0700

    first

--- para/src/App.jsx
+++ para/src/App.jsx
@@ -1,46 +1,57 @@
 
 
 
+
+
+
+
+
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+ function App(){
+
+const [ name, setPara,]=useState("")
+const [ btn, setPara2,]=useState("")
+function next(){
 
 
 
 
-
-function App() {
-  const [firstname, setfname,] = useState("")
-  const [lastnam, setlname,] = useState("")
-  const [fullname, setFullname] = useState("");
-
-  function name() {
-    setFullname(firstname + "" + lastnam)
-
-  }
-
-function clearInputs(){
-  setfname("")
-  setlname("")
-  setFullname("")
-}
-  return (
-    <>
-
-      <input type="text" value={firstname} onChange={(e) => setfname(e.target.value)} placeholder='Enter first name' />
-      <input type="text" value={lastnam} onChange={(e) => setlname(e.target.value)} placeholder='Enter last name' />
-
-
-      <button id='btn' onClick={name}>Click Me</button>
-      <button   id='btn2' onClick={clearInputs}>Clear</button> 
-      <h2>{fullname}</h2>
-
-
-    </>
-
-  )
-
+const reversed = name.split('').reverse().join('');
+setPara2(reversed);
 }
 
-export default App
+
+
+return(
+
+<>
+
+
+<input type="text" value={name} onChange={(e) => setPara(e.target.value)} placeholder='Enter firstname name' />
+
+<button onClick={next}>click</button>
+
+
+
+
+<h2>{btn}</h2>
+</>
+
+
+)
+
+
+
+
+
+ }
+
+
+
+
+
+
+ export default App
